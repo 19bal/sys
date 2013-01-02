e3a88ed83580967f35a71cd75a2ff334009b0499
nurdan tuğçe yeşilyurt <tugce.yesilyurt@bil.omu.edu.tr>
2013-01-02 18:45:50 +0200

    sys

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130102144442) do
+ActiveRecord::Schema.define(:version => 20130102150236) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -42,6 +42,9 @@ ActiveRecord::Schema.define(:version => 20130102144442) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "role"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "no"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
